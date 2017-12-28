8c203a21b2a9fdf0d031cfc76bd079b28d5a2443
mauro3g <mauro3g@hotmail.com>
2017-12-27 23:02:52 -0500

    Revert "Se agregaron pruebas y validaciones en el modulo de inicio de sesion"
    This reverts commit cc33be653f834a06c616580c0035f286cd0b86ae.

--- FinPres2/client/main.js
+++ FinPres2/client/main.js
@@ -2,7 +2,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import * as test_login from "./template/layout/layout_test.js";
 import * as searchTest from './template/search/searchTest.js';
 import * as searchMechanicsTest from './template/searchMechanics/searchMechanicsTest.js';
 
@@ -20,18 +19,6 @@ Meteor.startup(()=>{
     searchMechanicsTest. TestSearchMechanicNumbers();
     searchMechanicsTest.TestSearchMechanicLavado();
     searchMechanicsTest.TestSearchMechanicEmpty();
-    console.log("PRUEBAS DE LOGIN: ");
-    test_login.control_login_test_Mecanico();
-    test_login.control_login_test_Administrador();
-    test_login.control_login_test_usuarioNormal();
-    test_login.control_login_test_SinUsuario();
-    test_login.control_login_test_Admin_minuscula();
-    test_login.control_login_test_cedula_negativa();
-    test_login.control_login_test_cedula_caracter();
-    test_login.control_login_test_cedula_caracter_final();
-    test_login.control_login_test_admin_inicio();
-    test_login.control_login_test_admin_final();
-    test_login.control_login_test_entero_positivo_grande();
   },1000);
 
 
