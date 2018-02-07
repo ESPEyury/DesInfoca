c461942fc7f599883562cf921bec0ef9ec16e7a1
davidshaek <davidshaek@gmail.com>
2018-02-07 16:23:58 -0500

    IngSoftwareDavidLlerenaBadSmell

---
+++ FinPres2/client/template/mainAdmin/mainRoutes.js
@@ -0,0 +1,34 @@
+//David Llerena
+//Bad Smell - Lazy Class  I've added sesionAdministrador.js routes on an single implementation
+Template.mainServices.events({
+
+  'click #RegServ':function(event){
+    Router.go('/GesServ');
+    },
+  'click #ConsServ':function(event){
+    Router.go('/GesServCon');
+    },
+  'click #ModServ':function(event){
+    Router.go('/GesServMod');
+    },
+  'click #BuscSer':function(event){
+    Router.go('/');
+    },
+  'click #RevServ':function(event){
+    Router.go('/GesUser');
+    },
+
+  'click #RegMecani':function(event){
+    Router.go('/GesMen');
+    },
+  'click #ConsMeca':function(event){
+    Router.go('/GesMenCon');
+    },
+  'click #ModMeca':function(event){
+    Router.go('/GesMenMod');
+    },
+  'click #ConsUser':function(event){
+    Router.go('/GesCli');
+    },
+
+});
